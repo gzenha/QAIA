6fd4780ea945dd1ea5ae8e2a7fa1b80b8b4f0ecf
Gisele Gonçalves <giselepgoncalves@gmail.com>
2026-02-08 19:27:11 -0400

    Mundando a Descrição de Cenario, Apenas!

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('Teste deve estar online', async ({ page }) => {
   await page.goto('http://localhost:5173');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle('Velô by Papito');
-});
+}); 
