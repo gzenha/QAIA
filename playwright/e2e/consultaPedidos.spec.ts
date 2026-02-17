d4189f29a4af7d00cbf2d08ddf4d0606e0345db2
Gisele Gonçalves <giselepgoncalves@gmail.com>
2026-02-17 00:08:34 -0400

    validacoes de mensagens quando nao encontra pedido

--- playwright/e2e/consultaPedidos.spec.ts
+++ playwright/e2e/consultaPedidos.spec.ts
@@ -22,3 +22,39 @@ test('Consulta de Pedidos Aprovados', async ({ page }) => {
  // verificar se o pedido foi aprovado
    await expect(page.getByText('APROVADO')).toBeVisible();
 });
+
+test('Teste de validacao de item nao encontrado', async ({ page }) => {
+
+  const orderNumber = 'VLO-IGT123';
+
+  await page.goto('http://localhost:5173/');  
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+
+  await page.getByTestId('search-order-id').fill(orderNumber);
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click({timeout: 10000});
+
+//primeira forma de fazer funcionar  
+
+// checa se existe validacao 
+// await expect(page.locator('#root')).toContainText('Verifique o número do pedido');
+// await expect(page.locator('#root')).toContainText('Pedido não encontrado');
+
+// segunda forma de fazer funcionar 
+
+// const title = page.getByRole('heading', {name: 'Pedido não encontrado'});
+// await expect(title).toBeVisible({timeout:1000});
+//  const mensagem = page.getByText('Verifique o número do pedido e tente novamente');
+// const mensagem = page.locator('p', {hasText: 'Verifique o número do pedido e tente novamente'})
+// await expect(mensagem).toBeVisible({timeout: 10000}); 
+
+// terceira forma de fazer funcionar - snapchot
+await expect(page.locator('#root')).toMatchAriaSnapshot(`
+- img
+- heading "Pedido não encontrado" [level=3]
+- paragraph: Verifique o número do pedido e tente novamente
+`);
+
+
+
+});
